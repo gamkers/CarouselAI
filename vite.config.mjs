ec555787205ece5daca2458c8ae7560fbbe1c119
Akash M <gamkers@Akashs-Mac-mini.local>
2025-07-03 07:24:27 +0530

    first commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
-
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
+  base: '/', // Replace <repository-name> with your GitHub repository name
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
@@ -16,6 +25,13 @@ export default defineConfig({
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new'],
+    proxy: {
+      '/api': {
+        target: 'http://31.97.60.127:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   }
 });
